f6a48a52fad7c719da53994fe24209fd5d5e2ec5
paulo <paulo@paulo>
2024-12-26 18:47:09 -0300

    Ajustado script

--- tamanho_pasta.ps1
+++ tamanho_pasta.ps1
@@ -1,5 +1,31 @@
-$directory="C:\Users\331675\downloads"
+#$directory="C:\Users\331675\downloads"
 
-Get-ChildItem -force $directory -Directory -Recurse -ErrorAction SilentlyContinue | % { $_.fullname }
+#Get-ChildItem -force $directory -Directory -Recurse -ErrorAction SilentlyContinue | % { $_.fullname }
 
-Get-ChildItem –force $directory –Recurse -ErrorAction SilentlyContinue | measure Length -s -ErrorAction SilentlyContinue | Select-Object -Property @{name='Directory';expression={$directory}}, @{n="Size(GB)";e={[math]::Round((($_.Sum)/1GB),2)}}
+#Get-ChildItem -force $directory -Recurse -ErrorAction SilentlyContinue | measure Length -s -ErrorAction SilentlyContinue | Select-Object -Property @{name='Directory';expression={$directory}}, @{n="Size(GB)";e={[math]::Round((($_.Sum)/1GB),2)}}
+
+
+#Parameters
+$path = "C:\Users\331675\Downloads"
+$outputFile ="C:\Users\331675\FolderSize.csv"
+ 
+#Get all Folders, Sub-Folders recursively
+$folders = Get-ChildItem -Path $Path -Directory -Recurse
+ 
+#Loop through each folder to Find the size
+$FolderSizes = foreach ($folder in $folders) {
+    $size = (Get-ChildItem -Path $folder.FullName -File -Recurse | Measure-Object -Property Length -Sum).Sum
+    $sizeInMB = $size / 1GB
+ 
+    #Collect Data
+    [PSCustomObject]@{
+        FolderName = $folder.FullName
+        SizeInGB = [Math]::Round($sizeInMB,2)
+    }
+}
+#Export the Result to CSV
+$FolderSizes | Format-table
+$FolderSizes | Export-Csv -Path $outputFile -NoTypeInformation -Delimiter ';'
+
+
+#Read more: https://www.sharepointdiary.com/2021/07/get-folder-size-in-powershell.html#ixzz8uxJG1U00
